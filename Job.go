269a22fd3d3ae7263d571f9ae8e2673bed05c0ca
caselongo <caselongo@gmail.com>
2023-02-14 16:08:16 +0100

    added RunJob

--- Job.go
+++ Job.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	errortools "github.com/leapforce-libraries/go_errortools"
 	go_http "github.com/leapforce-libraries/go_http"
@@ -77,4 +78,134 @@ func (service *Service) GetJobs(config *GetJobsConfig) (*[]Job, *errortools.Erro
 	}
 
 	return &jobs, nil
+}
+
+type RunJobConfig struct {
+	AccountId              int64     `json:"-"`
+	JobId                  int64     `json:"-"`
+	Cause                  string    `json:"cause"`
+	GitSha                 *string   `json:"git_sha,omitempty"`
+	GitBranch              *string   `json:"git_branch,omitempty"`
+	SchemaOverride         *string   `json:"schema_override,omitempty"`
+	DbtVersionOverride     *string   `json:"dbt_version_override,omitempty"`
+	ThreadsOverride        *int64    `json:"threads_override,omitempty"`
+	TargetNameOverride     *string   `json:"target_name_override,omitempty"`
+	GenerateDocsOverride   *bool     `json:"generate_docs_override,omitempty"`
+	TimeoutSecondsOverride *int64    `json:"timeout_seconds_override,omitempty"`
+	StepsOverride          *[]string `json:"steps_override,omitempty"`
+}
+
+type RunJobResponse struct {
+	Data struct {
+		Id                 int       `json:"id"`
+		TriggerId          int       `json:"trigger_id"`
+		AccountId          int       `json:"account_id"`
+		ProjectId          int       `json:"project_id"`
+		JobDefinitionId    int       `json:"job_definition_id"`
+		Status             int       `json:"status"`
+		GitBranch          string    `json:"git_branch"`
+		GitSha             string    `json:"git_sha"`
+		StatusMessage      string    `json:"status_message"`
+		DbtVersion         string    `json:"dbt_version"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		DequeuedAt         time.Time `json:"dequeued_at"`
+		StartedAt          time.Time `json:"started_at"`
+		FinishedAt         time.Time `json:"finished_at"`
+		LastCheckedAt      time.Time `json:"last_checked_at"`
+		LastHeartbeatAt    time.Time `json:"last_heartbeat_at"`
+		OwnerThreadId      string    `json:"owner_thread_id"`
+		ExecutedByThreadId string    `json:"executed_by_thread_id"`
+		ArtifactsSaved     bool      `json:"artifacts_saved"`
+		ArtifactS3Path     string    `json:"artifact_s3_path"`
+		HasDocsGenerated   bool      `json:"has_docs_generated"`
+		Trigger            struct {
+			Id                     int       `json:"id"`
+			Cause                  string    `json:"cause"`
+			JobDefinitionId        int       `json:"job_definition_id"`
+			GitBranch              string    `json:"git_branch"`
+			GitSha                 string    `json:"git_sha"`
+			GithubPullRequestId    int       `json:"github_pull_request_id"`
+			SchemaOverride         string    `json:"schema_override"`
+			DbtVersionOverride     string    `json:"dbt_version_override"`
+			ThreadsOverride        int       `json:"threads_override"`
+			TargetNameOverride     string    `json:"target_name_override"`
+			GenerateDocsOverride   bool      `json:"generate_docs_override"`
+			TimeoutSecondsOverride int       `json:"timeout_seconds_override"`
+			StepsOverride          []string  `json:"steps_override"`
+			CreatedAt              time.Time `json:"created_at"`
+		} `json:"trigger"`
+		Job struct {
+			Id            int    `json:"id"`
+			AccountId     int    `json:"account_id"`
+			ProjectId     int    `json:"project_id"`
+			EnvironmentId int    `json:"environment_id"`
+			Name          string `json:"name"`
+			DbtVersion    string `json:"dbt_version"`
+			Triggers      struct {
+				GithubWebhook      bool `json:"github_webhook"`
+				GitProviderWebhook bool `json:"git_provider_webhook"`
+				Schedule           bool `json:"schedule"`
+				CustomBranchOnly   bool `json:"custom_branch_only"`
+			} `json:"triggers"`
+			ExecuteSteps []string `json:"execute_steps"`
+			Settings     struct {
+				Threads    int    `json:"threads"`
+				TargetName string `json:"target_name"`
+			} `json:"settings"`
+			State        int  `json:"state"`
+			GenerateDocs bool `json:"generate_docs"`
+			Schedule     struct {
+				Cron string `json:"cron"`
+				Date struct {
+					Type string `json:"type"`
+					Days []int  `json:"days"`
+					Cron string `json:"cron"`
+				} `json:"date"`
+				Time struct {
+					Type     string `json:"type"`
+					Interval int    `json:"interval"`
+					Hours    []int  `json:"hours"`
+				} `json:"time"`
+			} `json:"schedule"`
+		} `json:"job"`
+		Duration                string `json:"duration"`
+		QueuedDuration          string `json:"queued_duration"`
+		RunDuration             string `json:"run_duration"`
+		DurationHumanized       string `json:"duration_humanized"`
+		QueuedDurationHumanized string `json:"queued_duration_humanized"`
+		RunDurationHumanized    string `json:"run_duration_humanized"`
+		FinishedAtHumanized     string `json:"finished_at_humanized"`
+		StatusHumanized         string `json:"status_humanized"`
+		CreatedAtHumanized      string `json:"created_at_humanized"`
+	} `json:"data"`
+	/*Status struct {
+		Code             int    `json:"code"`
+		IsSuccess        bool   `json:"is_success"`
+		UserMessage      string `json:"user_message"`
+		DeveloperMessage string `json:"developer_message"`
+	} `json:"status"`*/
+}
+
+// RunJob runs a job
+//
+func (service *Service) RunJob(config *RunJobConfig) (*RunJobResponse, *errortools.Error) {
+	if config == nil {
+		return nil, errortools.ErrorMessage("config is nil")
+	}
+
+	var runJobResponse RunJobResponse
+
+	requestConfig := go_http.RequestConfig{
+		Method:        http.MethodPost,
+		Url:           service.url(fmt.Sprintf("accounts/%d/jobs/%d/run", config.AccountId, config.JobId)),
+		BodyModel:     config,
+		ResponseModel: &runJobResponse,
+	}
+	_, _, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	return &runJobResponse, nil
 }
--- go.mod
+++ go.mod
@@ -3,37 +3,39 @@ module github.com/leapforce-libraries/go_dbt
 go 1.18
 
 require (
-	github.com/leapforce-libraries/go_errortools v0.0.0-20211119145736-7b9b61c20618
-	github.com/leapforce-libraries/go_http v0.0.0-20220406191830-0235c3965bb8
-	github.com/leapforce-libraries/go_types v0.0.0-20220220161618-e9b1045c3ef2
-	github.com/leapforce-libraries/go_utilities v0.0.0-20220505202504-c3ea762c9a6f
+	github.com/leapforce-libraries/go_errortools v0.0.0-20230111113652-2e37de91e990
+	github.com/leapforce-libraries/go_http v0.0.0-20220808204824-8a0e0bdcdfd1
+	github.com/leapforce-libraries/go_types v0.0.0-20230209235725-e0c0b0948f2b
+	github.com/leapforce-libraries/go_utilities v0.0.0-20230124210046-7e78c8f41563
 )
 
 require (
-	cloud.google.com/go v0.101.1 // indirect
-	cloud.google.com/go/bigquery v1.31.0 // indirect
-	cloud.google.com/go/compute v1.6.0 // indirect
-	cloud.google.com/go/iam v0.3.0 // indirect
-	cloud.google.com/go/storage v1.22.0 // indirect
-	github.com/getsentry/sentry-go v0.9.0 // indirect
-	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	cloud.google.com/go v0.107.0 // indirect
+	cloud.google.com/go/bigquery v1.45.0 // indirect
+	cloud.google.com/go/compute v1.13.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
+	cloud.google.com/go/iam v0.8.0 // indirect
+	cloud.google.com/go/storage v1.28.1 // indirect
+	github.com/getsentry/sentry-go v0.16.0 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.7 // indirect
-	github.com/googleapis/gax-go/v2 v2.3.0 // indirect
-	github.com/googleapis/go-type-adapters v1.0.0 // indirect
-	github.com/leapforce-libraries/go_google v0.0.0-20220220163223-304493bbfe12 // indirect
-	github.com/leapforce-libraries/go_googlecloudstorage v0.0.0-20220220163255-cbfe4f06b7b6 // indirect
-	github.com/leapforce-libraries/go_integration v0.0.0-20220220163349-c13b3f3aa9d5 // indirect
-	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
-	google.golang.org/api v0.77.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/leapforce-libraries/go_google v0.0.0-20220622124815-b1621d319c1a // indirect
+	github.com/leapforce-libraries/go_googlecloudstorage v0.0.0-20220613120507-258e984f5c4f // indirect
+	github.com/leapforce-libraries/go_integration v0.0.0-20220801142518-7b4fe1b429bb // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.103.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
-	google.golang.org/grpc v1.46.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/genproto v0.0.0-20221202195650-67e5cbc046fd // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
